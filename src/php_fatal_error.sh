5094a10e0d3d490a550c162baa672a9e20eb7436
Nick Taylor <nrwtaylor@gmail.com>
2018-05-27 11:28:27 -0700

    Revert "May 2018 commit"
    This reverts commit 4272e2d619c014d2aaa04c9100edd2ea348d7eb7.

--- src/php_fatal_error.sh
+++
@@ -1,8 +0,0 @@
-# vim /root/scripts/php_fatal_error.sh
-#!/bin/sh
-
-# Grep PHP Fatal Error
-tail -n 100000 /var/log/apache2/error.log |awk -F'PHP Fatal error' '{print $2}'| grep -v '^$'| sort | uniq -c | sort -nr | head -n20| sed G > /tmp/php_fatal_error.log
-
-# send the email with the unix/linux mail command
-mail -s "PHP Fatal Error Report" nonnom < /tmp/php_fatal_error.log
