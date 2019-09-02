3d1fe56a43c083fd5fa40e39804f4f4d9d7e3fe6
Nick Taylor <nrwtaylor@gmail.com>
2019-09-02 15:05:54 -0700

    Googlelink

--- agents/Googlelink.php
+++ agents/Googlelink.php
@@ -55,13 +55,38 @@ class Googlelink extends Agent {
         // If we didn't receive the command NTP ...
 
         // Steps.
-        $ngram = new Ngram($this->thing, $this->subject);
+
+        // Identify parts of speech
+        $brilltagger = new Brilltagger($this->thing, $text);
+
+        $this->tokens = $brilltagger->tags;
+        $processed_text = "";
+        $exclude_parts_of_speech = array("IN", "CC", "VBZ");
+        foreach ($this->tokens as $index=>&$token) {
+            $token['use_flag'] = 1;
+            if (in_array($token["tag"], $exclude_parts_of_speech)) {$token['use_flag'] = 0;}
+        }
+
+
+
+        foreach ($this->tokens as $index=>&$token) {
+            if ($token["use_flag"] == 0) {continue;}
+
+            $processed_text .= $token['token'] . " ";
+
+        }
+
+
+        $text = $processed_text;
+        // Steps.
+        $ngram = new Ngram($this->thing, $text);
 
         $longest_string = "";
         $longest_string_length = 0;
         foreach (array(7, 6, 5, 4, 3, 2, 1) as $key=>$value) {
             $arr = $ngram->extractNgrams($text, $n = $value);
             if ($longest_string_length > 0) {break;}
+
             foreach ($arr as $key=>$value) {
                 // Measuring complexity.
                 if ($longest_string_length < strlen($value)) {
@@ -70,6 +95,9 @@ class Googlelink extends Agent {
                 }
             }
         }
+
+        if ($longest_string_length == 0) {$longest_string = $text;}
+
         // Turn it into this.
         // "https://www.google.com/search?q=google+hello"
         $m = mb_ereg_replace(" ", "+", $longest_string);
@@ -89,9 +117,20 @@ class Googlelink extends Agent {
      */
     public function readSubject() {
 
+        $input = $this->subject;
+        $whatIWant = $input;
+        if (($pos = strpos(strtolower($input), "google link")) !== FALSE) {
+            $whatIWant = substr(strtolower($input), $pos+strlen("google link"));
+        } elseif (($pos = strpos(strtolower($input), "googlelink")) !== FALSE) {
+            $whatIWant = substr(strtolower($input), $pos+strlen("googlelink"));
+        }
+
+        $filtered_input = ltrim(strtolower($whatIWant), " ");
+
+
         // Note intentional use of subject.
         // Must be passed without pre-processing
-        $this->doGooglelink($this->subject);
+        $this->doGooglelink($filtered_input);
 
         return false;
     }
