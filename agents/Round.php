33fc12a85d42e5539bdcdab44f8e1c2d4c72e1b2
Nick Taylor <nrwtaylor@gmail.com>
2020-12-09 18:15:42 -0800

    Initial build round generator.

---
+++ agents/Round.php
@@ -0,0 +1,685 @@
+<?php
+/**
+ * Round.php
+ *
+ * @package default
+ */
+
+namespace Nrwtaylor\StackAgentThing;
+
+//use QR_Code\QR_Code;
+
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(-1);
+
+use setasign\Fpdi;
+
+ini_set("allow_url_fopen", 1);
+
+class Round extends Agent
+{
+    public $var = 'hello';
+
+    /**
+     *
+     * @param Thing   $thing
+     * @param unknown $agent_input (optional)
+     */
+    public function init()
+    {
+        $this->test = "Development code";
+
+        $this->thing_report["info"] = "A ROUND is a repeating pattern.";
+        $this->thing_report["help"] = 'Click on the image for a PDF.';
+
+        $this->resource_path = $GLOBALS['stack_path'] . 'resources/';
+
+        $command_line = null;
+
+        $this->node_list = [
+            "round" => ["tzol\'kin", "haab", "day", "round", "week"],
+        ];
+
+        $this->current_time = $this->thing->json->time();
+
+        // Get some stuff from the stack which will be helpful.
+        $this->entity_name = $this->thing->container['stack']['entity_name'];
+
+        $this->default_canvas_size_x = 2000;
+        $this->default_canvas_size_y = 2000;
+
+        $agent = new Retention($this->thing, "retention");
+        $this->retain_to = $agent->retain_to;
+
+        $agent = new Persistence($this->thing, "persistence");
+        $this->time_remaining = $agent->time_remaining;
+        $this->persist_to = $agent->persist_to;
+
+        $this->default_rounds = [7];
+
+        $this->initRound();
+
+        $this->draw_center = false;
+        $this->draw_outline = false; //Draw hexagon line
+
+        $this->canvas_size_x = $this->default_canvas_size_x;
+        $this->canvas_size_y = $this->default_canvas_size_y;
+
+        $this->size = min(
+            $this->default_canvas_size_x,
+            $this->default_canvas_size_y
+        );
+    }
+
+    public function set()
+    {
+        $this->setRound();
+    }
+
+    /**
+     *
+     * @param unknown $input
+     */
+    function getWhatis($input)
+    {
+        $whatis = "round";
+        $whatIWant = $input;
+        if (($pos = strpos(strtolower($input), $whatis . " is")) !== false) {
+            $whatIWant = substr(
+                strtolower($input),
+                $pos + strlen($whatis . " is")
+            );
+        } elseif (($pos = strpos(strtolower($input), $whatis)) !== false) {
+            $whatIWant = substr(strtolower($input), $pos + strlen($whatis));
+        }
+
+        //$filtered_input = ltrim(strtolower($whatIWant), " ");
+        $filtered_input = ltrim($whatIWant, " ");
+
+        $this->whatis = $filtered_input;
+    }
+
+    /**
+     *
+     * @return unknown
+     */
+    public function respondResponse()
+    {
+        $this->thing->flagGreen();
+
+        $this->makeChoices();
+
+        if ($this->agent_input == null) {
+            $message_thing = new Message($this->thing, $this->thing_report);
+            $this->thing_report['info'] = $message_thing->thing_report['info'];
+        }
+    }
+
+    /**
+     *
+     */
+    public function makeChoices()
+    {
+        $this->choices = false;
+        $this->thing_report['choices'] = $this->choices;
+    }
+
+    /**
+     *
+     */
+    public function makeSMS()
+    {
+        //        $cell = $this->lattice[0][0][0];
+        $sms = "ROUND | ";
+        $sms .= $this->web_prefix . "thing/" . $this->uuid . "/round";
+        $sms .= " | " . $this->response;
+        $this->sms_message = $sms;
+        $this->thing_report['sms'] = $sms;
+    }
+
+    /**
+     *
+     */
+    public function makeMessage()
+    {
+        $message = "Made a round for you.<br>";
+
+        $uuid = $this->uuid;
+
+        $message .=
+            "Keep on stacking.\n\n<p>" .
+            $this->web_prefix .
+            "thing/$uuid/round.png\n \n\n<br> ";
+        $message .=
+            '<img src="' .
+            $this->web_prefix .
+            'thing/' .
+            $uuid .
+            '/round.png" alt="round" height="92" width="92">';
+
+        $this->thing_report['message'] = $message;
+    }
+
+    /**
+     *
+     */
+    public function setRound()
+    {
+        $this->thing->json->setField("variables");
+        $this->thing->json->writeVariable(["round", "rounds"], $this->rounds);
+    }
+
+    /**
+     *
+     * @return unknown
+     */
+    // TODO
+    public function getRound()
+    {
+        $this->thing->json->setField("variables");
+        $rounds = $this->thing->json->readVariable(["round", "rounds"]);
+
+        if ($rounds == false) {
+            $this->thing->log(
+                $this->agent_prefix . ' did not find rounds.',
+                "INFORMATION"
+            );
+            // No round saved.  Return.
+            return true;
+        }
+    }
+
+    /**
+     *
+     */
+    public function initRound()
+    {
+        if (!isset($this->size)) {
+            $this->size = 3.7;
+        }
+    }
+
+    public function run()
+    {
+    }
+
+    /**
+     *
+     */
+    public function makeWeb()
+    {
+        $link = $this->web_prefix . 'thing/' . $this->uuid . '/round.pdf';
+        $this->node_list = ["round" => ["week"]];
+        $web = "";
+        $web .= '<a href="' . $link . '">';
+        $web .= $this->html_image;
+        $web .= "</a>";
+        $web .= "<br>";
+
+        $this->thing_report['web'] = $web;
+    }
+
+    /**
+     *
+     */
+    public function makeTXT()
+    {
+        $txt = 'This is a ROUND';
+        $txt .= "\n";
+
+        $this->thing_report['txt'] = $txt;
+        $this->txt = $txt;
+    }
+
+    /**
+     *
+     * @param unknown $r
+     * @param unknown $g
+     * @param unknown $b
+     */
+    function rgbcolor($r, $g, $b)
+    {
+        $this->rgb = imagecolorallocate($this->image, $r, $g, $b);
+    }
+
+    /**
+     *
+     * @return unknown
+     */
+    public function makePNG()
+    {
+        if (isset($this->canvas_size_x)) {
+            $canvas_size_x = $this->canvas_size_x;
+            $canvas_size_y = $this->canvas_size_x;
+        } else {
+            $canvas_size_x = 164;
+            $canvas_size_y = 164;
+        }
+        $this->image = imagecreatetruecolor($canvas_size_x, $canvas_size_y);
+        //$this->image = imagecreatetruecolor(164, 164);
+
+        $this->white = imagecolorallocate($this->image, 255, 255, 255);
+        $this->black = imagecolorallocate($this->image, 0, 0, 0);
+        $this->red = imagecolorallocate($this->image, 255, 0, 0);
+        $this->green = imagecolorallocate($this->image, 0, 255, 0);
+        $this->grey = imagecolorallocate($this->image, 128, 128, 128);
+
+        $this->electric_red = imagecolorallocate($this->image, 231, 0, 0);
+        $this->dark_orange = imagecolorallocate($this->image, 255, 140, 0);
+        $this->canary_yellow = imagecolorallocate($this->image, 255, 239, 0);
+        $this->la_salle_green = imagecolorallocate($this->image, 0, 129, 31);
+        $this->blue = imagecolorallocate($this->image, 0, 68, 255);
+        $this->patriarch = imagecolorallocate($this->image, 118, 0, 137);
+
+        $this->flag_red = imagecolorallocate($this->image, 231, 0, 0);
+        $this->flag_orange = imagecolorallocate($this->image, 255, 140, 0);
+        $this->flag_yellow = imagecolorallocate($this->image, 255, 239, 0);
+        $this->flag_green = imagecolorallocate($this->image, 0, 129, 31);
+        $this->flag_blue = imagecolorallocate($this->image, 0, 68, 255);
+        // Indigo https://www.rapidtables.com/web/color/purple-color.html
+        $this->flag_indigo = imagecolorallocate($this->image, 75, 0, 130);
+        $this->flag_violet = imagecolorallocate($this->image, 118, 0, 137);
+        $this->flag_grey = $this->grey;
+
+        $this->indigo = imagecolorallocate($this->image, 75, 0, 130);
+
+        $this->ice_green = imagecolorallocate($this->image, 126, 217, 195);
+        $this->blue_ice = imagecolorallocate($this->image, 111, 122, 159);
+        $this->artic_ice = imagecolorallocate($this->image, 195, 203, 217);
+        $this->ice_cold = imagecolorallocate($this->image, 165, 242, 243);
+        $this->white_ice = imagecolorallocate($this->image, 225, 231, 228);
+
+        $this->ice_color_palette = [
+            $this->ice_green,
+            $this->blue_ice,
+            $this->artic_ice,
+            $this->ice_cold,
+            $this->white_ice,
+        ];
+
+        // Patriarch as a color name.
+        // https://www.schemecolor.com/lgbt-flag-colors.php
+        $this->color_palette = [
+            $this->electric_red,
+            $this->dark_orange,
+            $this->canary_yellow,
+            $this->la_salle_green,
+            $this->blue,
+            $this->patriarch,
+        ];
+
+        $this->flag_color_palette = [
+            $this->flag_red,
+            $this->flag_orange,
+            $this->flag_yellow,
+            $this->flag_green,
+            $this->flag_blue,
+            $this->flag_indigo,
+            $this->flag_violet,
+            $this->flag_grey,
+        ];
+
+        imagefilledrectangle(
+            $this->image,
+            0,
+            0,
+            $canvas_size_x,
+            $canvas_size_y,
+            $this->white
+        );
+
+        $textcolor = imagecolorallocate($this->image, 0, 0, 0);
+
+        $this->drawRounds();
+
+        // Write the string at the top left
+        $border = 30;
+        $r = 1.165;
+
+        $radius = ($r * ($canvas_size_x - 2 * $border)) / 3;
+
+        // devstack add path
+        $font = $this->resource_path . 'roll/KeepCalm-Medium.ttf';
+        $text = "A round in slices...";
+        // Add some shadow to the text
+        //imagettftext($image, 40, 0, 0, 75, $grey, $font, $number);
+
+        $size = $canvas_size_x - 90;
+        $size = 20;
+        $angle = 0;
+
+        $bbox = imagettfbbox($size, $angle, $font, $text);
+        $bbox["left"] = 0 - min($bbox[0], $bbox[2], $bbox[4], $bbox[6]);
+        $bbox["top"] = 0 - min($bbox[1], $bbox[3], $bbox[5], $bbox[7]);
+        $bbox["width"] =
+            max($bbox[0], $bbox[2], $bbox[4], $bbox[6]) -
+            min($bbox[0], $bbox[2], $bbox[4], $bbox[6]);
+        $bbox["height"] =
+            max($bbox[1], $bbox[3], $bbox[5], $bbox[7]) -
+            min($bbox[1], $bbox[3], $bbox[5], $bbox[7]);
+        extract($bbox, EXTR_PREFIX_ALL, 'bb');
+        //check width of the image
+        $width = imagesx($this->image);
+        $height = imagesy($this->image);
+        $pad = 0;
+        //        imagettftext($this->image, $size, $angle, $width/2-$bb_width/2, $height/2+ $bb_height/2, $this->black, $font, $text);
+
+        //imagestring($this->image, 2, 140, 0, $this->thing->nuuid, $textcolor);
+
+        // https://stackoverflow.com/questions/14549110/failed-to-delete-buffer-no-buffer-to-delete
+
+        if (ob_get_contents()) {
+            ob_clean();
+        }
+
+        ob_start();
+        imagepng($this->image);
+        $imagedata = ob_get_contents();
+
+        ob_end_clean();
+
+        $this->thing_report['png'] = $imagedata;
+
+        //echo '<img src="data:image/png;base64,'.base64_encode($imagedata).'"/>';
+        $response =
+            '<img src="data:image/png;base64,' .
+            base64_encode($imagedata) .
+            '"alt="round"/>';
+
+        $this->html_image =
+            '<img src="data:image/png;base64,' .
+            base64_encode($imagedata) .
+            '"alt="round"/>';
+
+        $this->PNG_embed = "data:image/png;base64," . base64_encode($imagedata);
+        $this->PNG = $imagedata;
+
+        return $response;
+
+        //        $this->PNG = $image;
+        //        $this->thing_report['png'] = $image;
+
+        //        return;
+    }
+
+    public function drawRounds()
+    {
+        $border = 100;
+        $size = 1000 - $border;
+
+        $round_width = $size / (count($this->rounds) + 1);
+
+        foreach ($this->rounds as $i => $round) {
+            $next_size = $size - $round_width;
+            if ($i == count($this->rounds) - 1) {
+                $next_size = 0;
+            }
+
+            $this->drawRound($round, $size, $next_size);
+            $size = $next_size;
+        }
+    }
+
+    /*
+     *
+     */
+
+    public function drawRound(
+        $n = 7,
+        $size = null,
+        $next_size = null,
+        $type = null
+    ) {
+        if ($type == null) {
+            $type = $this->type;
+        }
+        $this->wedgeRound($n, $size, $next_size);
+    }
+
+    public function wedgeRound($n = 7, $size = null, $next_size)
+    {
+        if ($size == null) {
+            $size = $this->size;
+        }
+
+        if (isset($this->canvas_size_x)) {
+            $canvas_size_x = $this->canvas_size_x;
+            $canvas_size_y = $this->canvas_size_y;
+        } else {
+            $canvas_size_x = $this->default_canvas_size_x;
+            $canvas_size_y = $this->default_canvas_size_y;
+        }
+
+        // Draw out the state
+        $center_x = $canvas_size_x / 2;
+        $center_y = $canvas_size_y / 2;
+
+        // devstack rotation not yet implemented
+        if (!isset($this->angle)) {
+            $this->angle = 0;
+        }
+        if ($n > 1) {
+            $init_angle = (-1 * pi()) / 2;
+            $angle = (2 * 3.14159) / $n;
+            //$x_pt =  230;
+            //$y_pt = 230;
+
+            foreach (range(0, $n - 1, 1) as $i) {
+                $x_pt = $size * cos($angle * $i + $init_angle);
+                $y_pt = $size * sin($angle * $i + $init_angle);
+                /*
+            imageline(
+                $this->image,
+                $center_x + $x_pt,
+                $center_y + $y_pt,
+                $center_x + $x_pt,
+                $center_y + $y_pt,
+                $this->black
+            );
+*/
+                $next_x_pt = $next_size * cos($angle * $i + $init_angle);
+                $next_y_pt = $next_size * sin($angle * $i + $init_angle);
+
+                imageline(
+                    $this->image,
+                    $center_x + $next_x_pt,
+                    $center_y + $next_y_pt,
+                    $center_x + $x_pt,
+                    $center_y + $y_pt,
+                    $this->black
+                );
+            }
+        }
+        imagearc(
+            $this->image,
+            $center_x,
+            $center_y,
+            2 * $size,
+            2 * $size,
+            0,
+            360,
+            $this->black
+        );
+    }
+
+    public function get()
+    {
+        $this->thing->json->setField("variables");
+        $time_string = $this->thing->json->readVariable([
+            "round",
+            "refreshed_at",
+        ]);
+
+        if ($time_string == false) {
+            $this->thing->json->setField("variables");
+            $time_string = $this->thing->json->time();
+            $this->thing->json->writeVariable(
+                ["round", "refreshed_at"],
+                $time_string
+            );
+        }
+    }
+
+    /**
+     *
+     * @return unknown
+     */
+    public function makePDF()
+    {
+        $this->getWhatis($this->subject);
+        try {
+            // initiate FPDI
+            $pdf = new Fpdi\Fpdi();
+
+            $pdf->setSourceFile($this->resource_path . 'snowflake/bubble.pdf');
+            $pdf->SetFont('Helvetica', '', 10);
+
+            $tplidx1 = $pdf->importPage(1, '/MediaBox');
+            $s = $pdf->getTemplatesize($tplidx1);
+
+            $pdf->addPage($s['orientation'], $s);
+            $pdf->useTemplate($tplidx1);
+
+            $pdf->Image($this->PNG_embed, 7, 30, 200, 200, 'PNG');
+
+            $pdf->SetTextColor(0, 0, 0);
+            $pdf->SetXY(1, 1);
+
+            $pdf->SetFont('Helvetica', '', 26);
+            $this->txt = "" . $this->whatis . ""; // Pure uuid.
+
+            $pdf->SetXY(140, 7);
+            $text = $this->whatis;
+            $line_height = 20;
+            $pdf->MultiCell(150, $line_height, $text, 0);
+
+            if (isset($this->hextile_PNG)) {
+                $top_x = -6;
+                $top_y = 11;
+
+                $pdf->Image(
+                    $this->hextile_PNG,
+                    $top_x,
+                    $top_y,
+                    -300,
+                    -300,
+                    'PNG'
+                );
+            }
+
+            // Page 2
+            $tplidx2 = $pdf->importPage(2);
+
+            $pdf->addPage($s['orientation'], $s);
+
+            $pdf->useTemplate($tplidx2, 0, 0);
+            // Generate some content for page 2
+
+            $pdf->SetFont('Helvetica', '', 10);
+            $this->txt = "" . $this->uuid . ""; // Pure uuid.
+
+            $link = $this->web_prefix . 'thing/' . $this->uuid . '/round';
+
+            $pdf->SetTextColor(0, 0, 0);
+
+            $pdf->SetXY(15, 7);
+
+            $line_height = 4;
+
+            $t = $this->thing_report['sms'];
+
+            $t = str_replace(" | ", "\n", $t);
+
+            $pdf->MultiCell(150, $line_height, $t, 0);
+
+            //$pdf->Link(15,7,150,10, $link);
+
+            $y = $pdf->GetY() + 0.95;
+            $pdf->SetXY(15, $y);
+            $text = "v0.0.1";
+            $pdf->MultiCell(
+                150,
+                $line_height,
+                $this->agent_name . " " . $text,
+                0,
+                "L"
+            );
+
+            $y = $pdf->GetY() + 0.95;
+
+            $pdf->SetXY(15, $y);
+            $text =
+                "Pre-printed text and graphics (c) 2020 " . $this->entity_name;
+            $pdf->MultiCell(150, $line_height, $text, 0, "L");
+
+            // Good until?
+            //$text = $this->timestampRound();
+            //$pdf->SetXY(175, 35);
+            //$pdf->MultiCell(30, $line_height, $text, 0, "L");
+
+            $image = $pdf->Output('', 'S');
+            $this->thing_report['pdf'] = $image;
+        } catch (Exception $e) {
+            echo 'Caught exception: ', $e->getMessage(), "\n";
+        }
+
+        return $this->thing_report['pdf'];
+    }
+
+    /**
+     *
+     */
+    public function readSubject()
+    {
+        $this->type = "wedge";
+        $input = strtolower($this->subject);
+
+        $number_agent = new Number($this->thing, "number");
+        $t = $number_agent->extractNumbers($input);
+
+        if ($t == []) {
+            $t = $this->default_rounds;
+        }
+
+        $this->rounds = $t;
+        $this->response .=
+            "Saw a request to make a round with these parameters: " .
+            implode(" ", $this->rounds) .
+            ". ";
+
+        $pieces = explode(" ", strtolower($input));
+
+        if (count($pieces) == 1) {
+            if ($input == 'round') {
+                $this->getRound();
+
+                $this->size = 4;
+                $this->response .= "Made a round. Which will pass. ";
+                return;
+            }
+        }
+
+        $input_agent = new Input($this->thing, "input");
+        $discriminators = ['wedge', 'slice'];
+        $input_agent->aliases['wedge'] = ['pizza', 'wheel', 'wedge'];
+        $input_agent->aliases['slice'] = ['slice', 'column', 'columns'];
+        $type = $input_agent->discriminateInput($input, $discriminators);
+        if ($type != false) {
+            $this->type = $type;
+        }
+
+        $keywords = ["uuid", "iterate", "pride", "flag", "hex"];
+        foreach ($pieces as $key => $piece) {
+            foreach ($keywords as $command) {
+                if (strpos(strtolower($piece), $command) !== false) {
+                    switch ($piece) {
+                        default:
+                    }
+                }
+            }
+        }
+
+        $this->getRound();
+    }
+}
