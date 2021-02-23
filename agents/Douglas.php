4f70d02d2cda98a2883a68b7e066d3fffdc68fb4
Nick <nrwtaylor@gmail.com>
2021-02-23 11:37:53 -0800

    Develop zip douglas.

--- agents/Douglas.php
+++ agents/Douglas.php
@@ -56,75 +56,69 @@ class Douglas extends Agent
         }
     }
 
-    public function rockyDouglas() {
-$thing = new Thing(null);
-$thing->Create("merp","merp","rocky");
-    $agent = new Rocky($thing, "rocky");
+    public function rockyDouglas()
+    {
+        $thing = new Thing(null);
+        $thing->Create("merp", "merp", "rocky");
+        $agent = new Rocky($thing, "rocky");
 
-    if (isset($messages[$agent->text])) {
-        $this->response .= "Duplicate detected. Skipped. ";
+        if (!isset($agent->thing_report["pdf"])) {
+            return true;
+        }
+
+        return [$thing->nuuid, $agent->thing_report["pdf"]];
     }
- //   $messages[$agent->text] = true;
 
-    if (!isset($agent->thing_report['pdf'])) {
-        $this->response .= "No PDF found. Skipped. ";
- //       continue;
-    }
-/*
-$zip = new \ZipArchive;
-var_dump($zip);
-*/
-/*
-$zipContent = null;
-$fp = tmpfile();
-fwrite($fp, $zipContent);
-$stream = stream_get_meta_data($fp);
-$filename = $stream['uri'];
-*/
+    public function zipDouglas()
+    {
+        $zip = new \ZipArchive();
 
-$zip = new \ZipArchive;
+        $ZIP_ERROR = [
+            \ZipArchive::ER_EXISTS => "File already exists.",
+            \ZipArchive::ER_INCONS => "Zip archive inconsistent.",
+            \ZipArchive::ER_INVAL => "Invalid argument.",
+            \ZipArchive::ER_MEMORY => "Malloc failure.",
+            \ZipArchive::ER_NOENT => "No such file.",
+            \ZipArchive::ER_NOZIP => "Not a zip archive.",
+            \ZipArchive::ER_OPEN => "Can't open file.",
+            \ZipArchive::ER_READ => "Read error.",
+            \ZipArchive::ER_SEEK => "Seek error.",
+        ];
+        $fp = tmpfile();
+        $stream = stream_get_meta_data($fp);
+        $filename = $stream["uri"];
 
+        $result_code = $zip->open($filename, \ZipArchive::CREATE);
+        //ls$result_code = $zip->open('/var/www/stackr.test/resources/douglas/test2.zip', \ZipArchive::CREATE);
 
-//$zip->open($filename);
+        if ($result_code !== true) {
+            $msg = isset($ZIP_ERROR[$result_code])
+                ? $ZIP_ERROR[$result_code]
+                : "Unknown error.";
+            $this->response .= $msg . " ";
+            return ["error" => $msg];
+        }
 
+        $this->response .= "Building zip file. ";
 
+        $sheet_count = 1;
+        if (isset($this->sheet_count)) {
+            $sheet_count = $this->sheet_count;
+        }
+        $this->response .= "Made " . $sheet_count . " sheets. ";
+        foreach (range(1, $sheet_count) as $i) {
+            [$nuuid, $pdf_string] = $this->rockyDouglas();
+            $zip->addFromString("rocky_" . $nuuid . ".pdf", $pdf_string);
+        }
+        $zip->close();
 
-$ZIP_ERROR = [
-  \ZipArchive::ER_EXISTS => 'File already exists.',
-  \ZipArchive::ER_INCONS => 'Zip archive inconsistent.',
-  \ZipArchive::ER_INVAL => 'Invalid argument.',
-  \ZipArchive::ER_MEMORY => 'Malloc failure.',
-  \ZipArchive::ER_NOENT => 'No such file.',
-  \ZipArchive::ER_NOZIP => 'Not a zip archive.',
-  \ZipArchive::ER_OPEN => "Can't open file.",
-  \ZipArchive::ER_READ => 'Read error.',
-  \ZipArchive::ER_SEEK => 'Seek error.',
-];
-$fp = tmpfile();
-$stream = stream_get_meta_data($fp);
-$filename = $stream['uri'];
-
-$result_code = $zip->open($filename, \ZipArchive::CREATE);
-//ls$result_code = $zip->open('/var/www/stackr.test/resources/douglas/test2.zip', \ZipArchive::CREATE);
-
-if( $result_code !== true ){
-   $msg = isset($ZIP_ERROR[$result_code])? $ZIP_ERROR[$result_code] : 'Unknown error.';
-$this->response .= $msg ." ";
-   return ['error'=>$msg];
-}
-
-$this->response .=  "Building zip file. ";
-
-    $zip->addFromString('rocky_asdfsa.pdf', $agent->thing_report['pdf']);
-
-    $zip->close();
-
-header('Content-type: application/zip');
-header('Content-Disposition: attachment; filename="download.zip"');
-$contents = file_get_contents($filename);
-
-file_put_contents('/var/www/stackr.test/resources/douglas/test5.zip', $contents);
-
+        header("Content-type: application/zip");
+        header('Content-Disposition: attachment; filename="download.zip"');
+        $contents = file_get_contents($filename);
+        file_put_contents(
+            "/var/www/stackr.test/resources/douglas/test5.zip",
+            $contents
+        );
     }
 
     /**
@@ -183,6 +177,14 @@ file_put_contents('/var/www/stackr.test/resources/douglas/test5.zip', $contents)
 
     public function readDouglas($text = null)
     {
+        if ($text === null) {
+            return;
+        }
+        $number = $this->extractNumber($text);
+
+        if (is_int($number) and $number >= 1) {
+            $this->sheet_count = $number;
+        }
     }
 
     public function run()
@@ -238,12 +240,18 @@ file_put_contents('/var/www/stackr.test/resources/douglas/test5.zip', $contents)
 
     public function readSubject()
     {
-        $input = strtolower($this->subject);
+        //        $input = strtolower($this->subject);
+        $input = $this->assert($this->input, "douglas", false);
+        //        $this->readDouglas($input);
 
-        $this->readDouglas($input);
+        $number = $this->extractNumber($input);
+        $number = intval($number);
+        if (is_int($number) and $number >= 1) {
+            $this->sheet_count = $number;
+        }
 
         $pieces = explode(" ", strtolower($input));
-$this->rockyDouglas();
+        $this->zipDouglas();
         if (count($pieces) == 1) {
             if ($input == "douglas") {
                 $this->getDouglas();
