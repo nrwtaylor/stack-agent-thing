9d06e7ec7f3c26341d849fe155df81cf3f31a422
Nick <nrwtaylor@gmail.com>
2021-03-27 16:35:54 -0700

    Review contextualweb and tidy.

--- agents/Identity.php
+++ agents/Identity.php
@@ -1,63 +1,68 @@
-var_dump<?php /**
+<?php /**
  * Identity.php
  *
  * @package default
  */
 
-
 namespace Nrwtaylor\StackAgentThing;
 
-ini_set('display_startup_errors', 1);
-ini_set('display_errors', 1);
+ini_set("display_startup_errors", 1);
+ini_set("display_errors", 1);
 error_reporting(-1);
 
 ini_set("allow_url_fopen", 1);
 
 class Identity extends Agent
 {
-
-    public $var = 'hello';
-
+    public $var = "hello";
 
     /**
      */
-    function init() {
-
-        $this->thing_report['help'] = 'This is your Identity. You can turn your Identity ON and OFF.';
+    function init()
+    {
+        $this->thing_report["help"] =
+            "This is your Identity. You can turn your Identity ON and OFF.";
 
         $this->keyword = "identity";
 
-        $this->node_list = array("identity"=>array("on"=>array("off")));
+        $this->node_list = ["identity" => ["on" => ["off"]]];
 
-        $this->variables_thing = new Variables($this->thing, "variables identity " . $this->from);
-
+        $this->variables_thing = new Variables(
+            $this->thing,
+            "variables identity " . $this->from
+        );
     }
-
 
     /**
      *
      */
-    function run() {
-
+    function run()
+    {
         $this->makeChoices();
     }
-
 
     /**
      *
      * @param unknown $requested_state (optional)
      */
-    function set($requested_state = null) {
-
+    function set($requested_state = null)
+    {
         if ($requested_state == null) {
-            if ((!isset($this->requested_state)) or ($this->requested_state == null)) {$this->requested_state = "X";}
+            if (
+                !isset($this->requested_state) or
+                $this->requested_state == null
+            ) {
+                $this->requested_state = "X";
+            }
             $requested_state = $this->requested_state;
-
         }
 
         $this->variables_thing->setVariable("state", $requested_state);
 
-        $this->variables_thing->setVariable("refreshed_at", $this->current_time);
+        $this->variables_thing->setVariable(
+            "refreshed_at",
+            $this->current_time
+        );
 
         $this->thing->choice->Choose($requested_state);
 
@@ -67,17 +72,22 @@ class Identity extends Agent
         $this->refreshed_at = $this->current_time;
     }
 
-
     /**
      *
      */
-    function get() {
+    function get()
+    {
+        $this->previous_state = $this->variables_thing->getVariable("state");
 
-        $this->previous_state = $this->variables_thing->getVariable("state")  ;
+        $this->refreshed_at = $this->variables_thing->getVariables(
+            "refreshed_at"
+        );
 
-        $this->refreshed_at = $this->variables_thing->getVariables("refreshed_at");
-
-        $this->thing->choice->Create($this->keyword, $this->node_list, $this->previous_state);
+        $this->thing->choice->Create(
+            $this->keyword,
+            $this->node_list,
+            $this->previous_state
+        );
 
         $this->state = $this->thing->choice->current_node;
 
@@ -90,14 +100,13 @@ class Identity extends Agent
      * @param unknown $choice (optional)
      * @return unknown
      */
-    function selectChoice($choice = null) {
-
+    function selectChoice($choice = null)
+    {
         if ($choice == null) {
             return $this->state;
 
             //        $choice = 'off'; // Fail off.
         }
-
 
         $this->thing->log('"chose "' . $choice . '".');
 
@@ -108,24 +117,22 @@ class Identity extends Agent
         return $this->state;
     }
 
-
     /**
      *
      */
-    function makeChoices() {
-
-        $choices = $this->variables_thing->thing->choice->makeLinks($this->state);
-        $this->thing_report['choices'] = $choices;
-
-
+    function makeChoices()
+    {
+        $choices = $this->variables_thing->thing->choice->makeLinks(
+            $this->state
+        );
+        $this->thing_report["choices"] = $choices;
     }
 
-
     /**
      *
      */
-    function makeSMS() {
-
+    function makeSMS()
+    {
         if ($this->state == false) {
             $t = "X";
         } else {
@@ -137,8 +144,9 @@ class Identity extends Agent
             $sms_message .= " | identity " . strtoupper($this->from);
         }
 
-        $sms_message .= " | nuuid " . substr($this->variables_thing->variables_thing->uuid, 0, 4);
-
+        $sms_message .=
+            " | nuuid " .
+            substr($this->variables_thing->variables_thing->uuid, 0, 4);
 
         if ($this->state == "off") {
             $sms_message .= " | TEXT IDENTITY ON";
@@ -146,32 +154,33 @@ class Identity extends Agent
             $sms_message .= " | TEXT ?";
         }
 
-        $this->thing_report['sms'] = $sms_message;
+        $this->thing_report["sms"] = $sms_message;
     }
-
 
     /**
      *
      */
-    function makeWeb() {
-
-        $this->thing_report['web'] = "Web identity is not available in this channel.";
-
+    function makeWeb()
+    {
+        $this->thing_report["web"] =
+            "Web identity is not available in this channel.";
     }
 
     /**
      *
      * @return unknown
      */
-    public function readSubject() {
+    public function readSubject()
+    {
         $this->response = null;
 
-        $keywords = array('off', 'on');
+        $keywords = ["off", "on"];
 
         $input = strtolower($this->subject);
 
         // Because the identity is likely to be in the from address
-        $haystack = $this->agent_input . " " . $this->from . " " . $this->subject;
+        $haystack =
+            $this->agent_input . " " . $this->from . " " . $this->subject;
 
         //  $this->requested_state = $this->discriminateInput($haystack); // Run the discriminator.
 
@@ -179,11 +188,9 @@ class Identity extends Agent
 
         $pieces = explode(" ", strtolower($input));
 
-
         // So this is really the 'sms' section
         // Keyword
         if (count($pieces) == 1) {
-
             if ($input == $this->keyword) {
                 //                $this->read();
                 return;
@@ -192,31 +199,28 @@ class Identity extends Agent
             // Drop through to piece scanner
         }
 
-
-        foreach ($pieces as $key=>$piece) {
+        foreach ($pieces as $key => $piece) {
             foreach ($keywords as $command) {
                 if (strpos(strtolower($piece), $command) !== false) {
                     switch ($piece) {
-                    case 'off':
-                        $this->thing->log('switch off');
-                        $this->selectChoice('off');
-                        return;
-                    case 'on':
-                        $this->selectChoice('on');
-                        return;
-                    case 'next':
-                    default:
+                        case "off":
+                            $this->thing->log("switch off");
+                            $this->selectChoice("off");
+                            return;
+                        case "on":
+                            $this->selectChoice("on");
+                            return;
+                        case "next":
+                        default:
                     }
-
                 }
             }
-
         }
 
-$discriminators = [
-'on' => ['red', 'on'],
-'off' => ['green', 'off']
-];
+        $discriminators = [
+            "on" => ["red", "on"],
+            "off" => ["green", "off"],
+        ];
 
         $type = $this->discriminateInput($input, $discriminators);
         if ($type != false) {
@@ -225,20 +229,17 @@ $discriminators = [
 
         // If all else fails try the discriminator.
         switch ($this->requested_state) {
-        case 'on':
-            $this->selectChoice('on');
-            return;
-        case 'off':
-            $this->selectChoice('off');
-            return;
+            case "on":
+                $this->selectChoice("on");
+                return;
+            case "off":
+                $this->selectChoice("off");
+                return;
         }
 
         // Don't read anything else.
         //        $this->read();
 
         return "Message not understood";
-
     }
-
-
 }
