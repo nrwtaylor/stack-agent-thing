11c57525fdd49de0035c78fa4c53c20da70afe05
nrwtaylor <nrwtaylor@gmail.com>
2021-10-26 17:53:40 -0700

    Develop hex flake

--- agents/Glossary.php
+++ agents/Glossary.php
@@ -16,6 +16,7 @@ class Glossary extends Agent
      */
     function init()
     {
+
         $this->thing_report["agency"] =
             "Prepare a helpful glossary of ALL stack agents.";
         $this->thing_report["info"] =
